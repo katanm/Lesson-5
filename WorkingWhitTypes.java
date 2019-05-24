a893025254b04a498847a41d711cc42e5f8751d8
katanm <mihai.katan@gmail.com>
2019-05-24 18:28:10 +0300

    HomeWork Lesson 5 +*class

---
+++ WorkingWhitTypes.java
@@ -0,0 +1,41 @@
+public class WorkingWhitTypes {
+
+	public static void main (String[] args){
+
+		// tip denumire (= valoare)
+		// 2 Declararea variabelelor
+		
+		int itemLenght;
+		double weight;
+
+		// 2.1 Initializarea variabilelor;
+		
+		max = 12.5;
+
+		// 2.2 Constanta
+		
+		max = 1+2+3+4+5+6+7+ithemLenght;
+
+		boolean yes = true;
+		boolean no = false;
+
+		// char 
+		char letter = 'a';
+		char letterA = 97;
+		String text = "'Hello World'"
+
+		// Modulus
+		int x=5, y=6;
+		int modulo = y % x;
+
+		int diff = y-x;
+		int multiplication = y*x;
+
+		// Double
+		double x = 13.5;
+		float fl = 13.5F;
+		
+
+	}
+
+}
